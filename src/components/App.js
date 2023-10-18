ce2350e652a0c200bef8a917ee22f67a42e51cbd
havr1985 <gavrtel1985@gmail.com>
2023-10-18 10:41:34 +0300

    added localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,21 @@ export class App extends Component {
     filter:'',
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('user-contacts');
+    if (savedContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(savedContacts)
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('user-contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = newContact => {
     const check = this.state.contacts.some(({ name }) => newContact.name === name);
     if (check) {
